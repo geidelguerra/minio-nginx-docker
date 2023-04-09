6526d61382525667c83f81c6ba28d775563717b0
Geidel Guerra <cayohuesodev@pm.me>
2023-04-09 16:48:51 -0400

    wip

---
+++ gen-certs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -it --rm --name=certbot -p 80:80 -p 443:443 -v '/etc/letsencrypt:/etc/letsencrypt' -v '/var/lib/letsencrypt:/var/lib/letsencrypt' certbot/certbot certonly --standalone --agree-tos -d "$@"
